36afd25c909fcd3f23bad5a8ec8cc29ed2cef7f6
Sam <SamuelWare0@gmail.com>
2018-09-03 23:54:46 +0100

    other polish

--- Scripts/Usables/Gravestone.gd
+++ Scripts/Usables/Gravestone.gd
@@ -1,6 +1,9 @@
 extends "res://Scripts/Usables/Usable.gd"
 
 var points = 0
+var velocity = Vector2()
+
+onready var global = $"/root/Global"
 
 func _init():
 	text = "Recover"
@@ -8,3 +11,7 @@ func _init():
 func activate():
 	$"../AnimationPlayer".play("Descent")
 	player.alter_points(points)
+
+func _physics_process(delta):
+	velocity.y += global.GRAVITY * delta
+	velocity = get_parent().move_and_slide(velocity)
